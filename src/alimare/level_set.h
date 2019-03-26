b48a29244df3e0c2ac6d036a202e49805a27d596
Alexandre Limare <limarealexandre@gmail.com>
2019-03-26 16:21:44 +0100

    debugging of level_set.h

--- src/alimare/level_set.h
+++ src/alimare/level_set.h
@@ -11,25 +11,10 @@ The level set function is defined and initialized elsewhere (typically by the
 user), the face vector field `uf` and the timestep `dt` are defined by a
 solver. */
 
-extern scalar * LS;
+extern scalar * level_set;
 extern face vector uf;
 extern double dt;
 
-/**
-On adaptive meshes, the Level Set needs to use linear interpolation (rather
-than the default bilinear interpolation) to ensure conservation when
-refining cells. */
-
-#if TREE
-event defaults (i = 0) {
-#if EMBED
-    LS.refine = LS.prolongation = refine_embed_linear;
-#else
-    LS.refine  = refine_linear;
-#endif
-    LS.restriction = restriction_volume_average;
-}
-#endif
 
 /**
 The integration is performed using the Bell-Collela-Glaz scheme. */
@@ -37,7 +22,8 @@ The integration is performed using the Bell-Collela-Glaz scheme. */
 #include "bcg.h"
 
 event LS_advection (i++,last) {
-  advection (LS, uf, dt);
+  // printf("%d \n", i);
+  advection (level_set, uf, dt);
 }
 /**
 Reinitialization method can be chosen by overloading this function */
