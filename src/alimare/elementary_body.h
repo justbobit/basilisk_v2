a454d95ea15d960d8c9f796e526afa6e93c0da9b
LIMARE Alexandre <limare@blum.polytechnique.fr>
2019-04-11 15:17:10 +0200

    documentation of the reinit2 function

--- src/alimare/elementary_body.h
+++ src/alimare/elementary_body.h
@@ -300,7 +300,17 @@ double sign2 (double x)
 }
 /**
 V2 of the reinit function with subcell correction.
+Based on the work of Russo2000.
+phi^n+1 = phi^n - Delta t S(phi) G(phi) far from the interface
+Near the interface it is modified to :
+phi^n+1 = phi^n - Delta t/Delta x ( sgn(phi^0) |phi^n| - Di)
 
+with:
+Di = Delta x * phi_i^0/Delta phi_0^i
+
+with:  
+Delta phi_0^i = max((phi^0_{i-1}-phi^0_{i+1})/2,phi^0_{i-1}-phi^0_{i},
+phi^0_{i}-phi^0_{i+1})
 */
 
 
